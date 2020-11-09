37c818bd7373eceeeb0f53757160d1de42d973d6
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-09 12:18:39 +0000

    other methodas added

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ account_2 = BankAccount("Osian", 1000, "personal")
 print(account.holder_name)
 print(account_2.holder_name)
 
+account_2.holder_name = "Sarah"
+print(account_2.holder_name)
+
+print("balance before pay_in: ", account_2.balance)
+account_2.pay_in(250)
+print(f"balance after pay_in: {account_2.balance}")
+
+
+print("balance before pay_monthly_fee: ", account_2.balance)
+account_2.pay_monthly_fee()
+print(f"balance after pay_monthly_fee: {account_2.balance}")
--- modules/bank_account.py
+++ modules/bank_account.py
@@ -1,11 +1,21 @@
 class BankAccount:
 
     # constructor method - if you needs to pass data to the class.
-    # self needs to be the first parameter!
-    def __init__(self, input_holder_name, input_amount, input_type):
+    # self needs to be the first parameter in ALL methods in a class!
+    # self needs to be the first parameter in ALL methods in a class!
+    # self needs to be the first parameter in ALL methods in a class!
+    def __init__(self, input_holder_name, input_balance, input_type):
 
-        #instance variable
+        #instance variables
         self.holder_name = input_holder_name
-        self.amount = input_amount
+        self.balance = input_balance
         self.type = input_type
 
+
+    # methods: functions in classes
+    def pay_in(self, amount):
+        self.balance += amount
+
+
+    def pay_monthly_fee(self):
+        self.balance -= 50
