88c5385efbf36e61060775aa9a8b6a2250aa3032
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-09 11:55:47 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from modules.bank_account import BankAccount
+
+account = BankAccount("John", 500, "business")
+
+print(account)
+
---
+++ modules/bank_account.py
@@ -0,0 +1,6 @@
+class BankAccount:
+
+    # constructor method - self needs to be the first parameter!
+    def __init__(self, input_holder_name, input_amount, input_type):
+        pass
+
